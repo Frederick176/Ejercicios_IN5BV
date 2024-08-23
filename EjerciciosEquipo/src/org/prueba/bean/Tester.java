342ef9971a9e9dc017a3614736741cfe2250b58b
Frederick176 <fhernandez-2023176@kinal.edu.gt>
2024-08-23 16:22:44 -0600

    add(ejercicio1-jtojin):se agregaron archivos para Empleado

---
+++ EjerciciosEquipo/src/org/prueba/bean/Tester.java
@@ -0,0 +1,13 @@
+
+package org.prueba.bean;
+
+/**
+ *
+ * Nombre : Johan Miguel Tojin Acabal
+ * Grado:   5to Perito Informatica
+ * Seccion: IN5BV
+ * Carnet:  2020591
+ */
+public class Tester {
+    
+}
