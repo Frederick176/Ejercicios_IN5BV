cc56fe84fd230fd75343d3c44a8c014e2269583a
Frederick176 <fhernandez-2023176@kinal.edu.gt>
2024-08-23 15:29:32 -0600

    build(ejercicio1-jtojin):iniciacion de Proyecto

---
+++ EjerciciosEquipo/src/org/prueba/system/Main.java
@@ -0,0 +1,20 @@
+
+package org.prueba.system;
+
+/**
+ *
+ * Nombre :
+ * Grado:
+ * Seccion:
+ * Carnet:
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba Diagnostica");
+    }
+    
+}
