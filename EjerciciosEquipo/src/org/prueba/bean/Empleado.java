84d2874a62da4c0c4798fe2a332c4f1359805854
Frederick176 <fhernandez-2023176@kinal.edu.gt>
2024-08-23 16:35:17 -0600

    feat(ejercicio1-jtojin): se agrego constructor vacio y lleno

--- EjerciciosEquipo/src/org/prueba/bean/Empleado.java
+++ EjerciciosEquipo/src/org/prueba/bean/Empleado.java
@@ -14,6 +14,47 @@ public abstract class Empleado {
     private String nombre;
     private int edad;
     private double salario;
+
+    
+    // constructor vacio 
+    public Empleado() {
+    }
+
+    
+    // constructor lleno
+    public Empleado(String nombre, int edad, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+    
     
      // Método abstracto trabajar
     abstract void trabajar();
@@ -22,4 +63,6 @@ public abstract class Empleado {
     void trabajar(int horas) {
         System.out.println("horas trabajadas.");
     }
+    
+    
 }
